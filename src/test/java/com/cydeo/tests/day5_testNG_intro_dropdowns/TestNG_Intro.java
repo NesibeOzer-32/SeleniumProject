50ace71c3f8b794c48f65f88adfd5979e13ef6f9
Nesibe Ozer <kosucunesibe@gmail.com>
2022-09-04 23:13:40 +0200

    day5_testNG_intro_dropdowns

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
@@ -1,9 +1,19 @@
 package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestNG_Intro {
 
+    @BeforeMethod
+    public void setUpMethod(){
+        System.out.println("---> Before Method is running!");
+    }
+    @AfterMethod
+    public void  tearDownMethod(){
+        System.out.println("---> After Method is running!");
+    }
     @Test
     public void test1(){
         System.out.println("Test 1 is running...");
