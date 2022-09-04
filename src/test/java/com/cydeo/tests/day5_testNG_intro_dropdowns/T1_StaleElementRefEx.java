8e2691c671733a6b771545c2199abfe69352d3e4
Nesibe Ozer <kosucunesibe@gmail.com>
2022-09-04 19:57:34 +0200

    day5_testNG_intro_dropdowns

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T1_StaleElementRefEx.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T1_StaleElementRefEx.java
@@ -2,13 +2,14 @@ package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.util.concurrent.TimeUnit;
 
 public class T1_StaleElementRefEx {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         //1-Open chrome browser
         WebDriver driver= WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
@@ -19,12 +20,30 @@ public class T1_StaleElementRefEx {
 
         //3-Click to add elements button
         WebElement addElementsBtn = driver.findElement(By.xpath("//button[@onclick='addElement()']"));
+        Thread.sleep(2000);
         addElementsBtn.click();
 
         //4-Verify "Delete" button is displayed after clicking
+        WebElement deleteBtn= driver.findElement(By.xpath("//div[@id='elements']"));
+        System.out.println("deleteBtn.isDisplayed() = " + deleteBtn.isDisplayed());
 
         //5-Click to "Delete" button
+        deleteBtn.click();
+
         //6-Verify "Delete" button is NOT displayed after clicking
-        //USE XPATH LOCATOR FOR ALL WEB ELEMENT LOCATORS
+
+        try{
+                 System.out.println("deleteBtn.isDisplayed() = "+ deleteBtn.isDisplayed());
+        }catch (StaleElementReferenceException e){
+                 System.out.println("--> StaleElementReferenceException exception is thrown");
+                 System.out.println("--> This means the web element is completely deleted from the page ");
+                 System.out.println("--> deleteBtn.isDisplayed() = false");
+
+        }
+
+        //deleteBtn= driver.findElement(By.xpath("//div[@id='elements']"));
+
+        driver.close();
     }
 }
+//USE XPATH LOCATOR FOR ALL WEB ELEMENT LOCATORS
