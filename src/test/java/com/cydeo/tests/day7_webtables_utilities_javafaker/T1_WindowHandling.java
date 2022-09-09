571bf8c9e7d768e04173bfc3ad0afb0e52b9ec23
Nesibe Ozer <kosucunesibe@gmail.com>
2022-09-09 14:34:56 +0200

    day7_webtables_utilities_javafaker

--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T1_WindowHandling.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T1_WindowHandling.java
@@ -1,5 +1,6 @@
 package com.cydeo.tests.day7_webtables_utilities_javafaker;
 
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -32,31 +33,10 @@ public class T1_WindowHandling {
         ((JavascriptExecutor) driver).executeScript("window.open('https://etsy.com','_blank');");
         ((JavascriptExecutor) driver).executeScript("window.open('https://facebook.com','_blank');");
 
-        
-
         //4. Create a logic to switch to the tab where Etsy.com is
 
-        Set <String> allWindowsHandles= driver.getWindowHandles();
+        BrowserUtils.switchWindowAndVerify(driver, "etsy", "Etsy");
 
-        for (String each:allWindowsHandles ){
-            driver.switchTo().window(each);
-
-            System.out.println("current URL: " + driver.getCurrentUrl());
-            if(driver.getCurrentUrl().contains("etsy")){
-                break;
-            }
-        }
-
-
-
-        //5.Assert:Title contains"Etsy"
-        String actualTitle= driver.getTitle();
-        String expectedInTitle= "Etsy";
-
-        Assert.assertTrue(actualTitle.contains(expectedInTitle));
-
-
-        //Lines to be pasted
 
     }
 }
