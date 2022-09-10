cd47061a5f203d6aae84b30693992a2723b42ccb
Nesibe Ozer <kosucunesibe@gmail.com>
2022-09-10 15:31:55 +0200

    day8_properties_config_reader

--- src/test/java/com/cydeo/tests/day8_properties_config_reader/LearningProperties.java
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/LearningProperties.java
@@ -7,6 +7,11 @@ public class LearningProperties {
     @Test
     public void java_properties_reading_test(){
 
-        System.getProperty("os.name");
+        //key= value
+        //os.name=Windows 10
+        //user.name=kantoor
+        System.out.println("System.getProperty(\"os.name\") = " + System.getProperty("os.name"));
+        System.out.println("System.getProperty(\"user.name\") = " + System.getProperty("user.name"));
+
     }
 }
