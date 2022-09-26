27f1fd3bf36898fdf0e29022505d959232cd4452
Nesibe Ozer <kosucunesibe@gmail.com>
2022-09-26 09:47:23 +0200

    day11_Driver_Close - Actions - Jsexecutor

--- src/test/java/com/cydeo/tests/day11_Driver_Close_Actions_Jsexecutor/T1_Circle_Drag_N_Drop.java
+++ src/test/java/com/cydeo/tests/day11_Driver_Close_Actions_Jsexecutor/T1_Circle_Drag_N_Drop.java
@@ -3,6 +3,8 @@ package com.cydeo.tests.day11_Driver_Close_Actions_Jsexecutor;
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class T1_Circle_Drag_N_Drop {
@@ -22,11 +24,17 @@ public class T1_Circle_Drag_N_Drop {
         WebElement bigCircle= Driver.getDriver().findElement(By.id("droptarget"));
 
         //2. Drag and drop the small circle to bigger circle.
+        Actions actions= new Actions(Driver.getDriver());
+        //actions.dragAndDrop(smallCircle,bigCircle).perform();
+        actions.clickAndHold(smallCircle).moveToElement(bigCircle).release().perform();
 
-        //actions.dragAndDrop(smallCircle, bigCircle).perform();
 
         //3. Assert:
         //-Text in big circle changed to: “You did great!”
+        String actualBigCircleText=bigCircle.getText();
+        String expectedBigCircleText= "You did great!";
+
+        Assert.assertEquals(actualBigCircleText,expectedBigCircleText);
 
 
     }
