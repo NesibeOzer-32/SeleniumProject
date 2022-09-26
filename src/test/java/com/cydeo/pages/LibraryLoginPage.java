1723fd7377b4cb3ccc6cadc0b7060bce66781380
Nesibe Ozer <kosucunesibe@gmail.com>
2022-09-26 11:19:03 +0200

    day12_LibraryLoginPage

--- src/test/java/com/cydeo/pages/LibraryLoginPage.java
+++ src/test/java/com/cydeo/pages/LibraryLoginPage.java
@@ -1,13 +1,24 @@
 package com.cydeo.pages;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LibraryLoginPage {
 
-    //#-initialize the driver instance and object of the class
+    //#1-initialize the driver instance and object of the class
     public LibraryLoginPage(){
+        /*initElements method will create connection in between the current deriver sessioin (instance)
+        and the object of the current class.
+        */
         PageFactory.initElements(Driver.getDriver(),this);
 
     }
+    //#2-use @FindBy annotation to locate web elements
+
+    @FindBy(xpath = "//input[@id='inputEmail']")
+    public WebElement inputUsername;
+
+
 }
