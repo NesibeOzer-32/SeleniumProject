4eb2722b76897006e4f7ea581f2dd238f58d996b
Nesibe Ozer <kosucunesibe@gmail.com>
2022-09-26 09:14:23 +0200

    day11_Driver_Close - Actions - Jsexecutor

---
+++ src/test/java/com/cydeo/tests/day11_Driver_Close_Actions_Jsexecutor/T1_Circle_Drag_N_Drop.java
@@ -0,0 +1,33 @@
+package com.cydeo.tests.day11_Driver_Close_Actions_Jsexecutor;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class T1_Circle_Drag_N_Drop {
+
+    @Test
+    public void drag_and_drop_test(){
+
+        //1. Go to https://demos.telerik.com/kendo-ui/dragdrop/index
+        Driver.getDriver().get("https://demos.telerik.com/kendo-ui/dragdrop/index");
+
+        //Locate "Accept Cookies" button and click
+        WebElement acceptCookiesBtn = Driver.getDriver().findElement(By.id("onetrust-accept-btn-handler"));
+        acceptCookiesBtn.click();
+
+        //Locate small and big circles to be able to drag them around
+        WebElement smallCircle= Driver.getDriver().findElement(By.id("draggable"));
+        WebElement bigCircle= Driver.getDriver().findElement(By.id("droptarget"));
+
+        //2. Drag and drop the small circle to bigger circle.
+
+        //actions.dragAndDrop(smallCircle, bigCircle).perform();
+
+        //3. Assert:
+        //-Text in big circle changed to: “You did great!”
+
+
+    }
+}
