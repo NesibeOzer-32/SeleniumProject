1bf17fe90cf73bcad8a266421bbd6e37495eb95d
Nesibe Ozer <kosucunesibe@gmail.com>
2022-09-28 10:04:25 +0200

    day13_review_and_practices

--- src/test/java/com/cydeo/tests/day13_review_and_practices/ExplicitWaitPractices.java
+++ src/test/java/com/cydeo/tests/day13_review_and_practices/ExplicitWaitPractices.java
@@ -2,6 +2,7 @@ package com.cydeo.tests.day13_review_and_practices;
 
 import com.cydeo.pages.DoubleClickPage;
 import com.cydeo.pages.DynamicControlsPage;
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.interactions.Actions;
@@ -31,10 +32,11 @@ public class ExplicitWaitPractices {
         dynamicControlsPage.removeButton.click();
 
         //4- Wait until “loading bar disappears”
-        Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        WebDriverWait wait=new WebDriverWait(Driver.getDriver(),10);
-        wait.until(ExpectedConditions.invisibilityOf(dynamicControlsPage.loadingBar));
-        Driver.getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        //WebDriverWait wait=new WebDriverWait(Driver.getDriver(),10);
+        //wait.until(ExpectedConditions.invisibilityOf(dynamicControlsPage.loadingBar));
+        //Ustteki uc sira yerine utilitydeki methodla  tek satir kodla
+        BrowserUtils.waitForInvisibilityOf(dynamicControlsPage.loadingBar);
 
         //5- Verify:
         //a. Checkbox is not displayed
@@ -52,6 +54,16 @@ public class ExplicitWaitPractices {
 
     }
 
+    @Test
+    public void enable_button_test(){
+
+        //3- Click to “Enable” button
+        //4- Wait until “loading bar disappears”
+        //5- Verify:
+        //a. Input box is enabled.
+        //b. “It’s enabled!” message is displayed.
+
+    }
 
 
 }
