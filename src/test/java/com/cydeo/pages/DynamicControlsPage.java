2d073155dfa0f0354281e9a84146716fae712b02
Nesibe Ozer <kosucunesibe@gmail.com>
2022-09-28 09:39:36 +0200

    day13_review_and_practices

---
+++ src/test/java/com/cydeo/pages/DynamicControlsPage.java
@@ -0,0 +1,28 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilities.Driver;
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class DynamicControlsPage {
+
+    public DynamicControlsPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//button[@onclick='swapCheckbox()']")
+    public WebElement removeButton;
+
+    @FindBy(xpath = "//img[@src='/img/ajax-loader.gif']")
+    public WebElement loadingBar;
+
+    @FindBy(xpath = "//input[@id='checkbox']")
+    public WebElement checkBox;
+
+    @FindBy(xpath = "//p[@id='message']")
+    public WebElement message;
+
+
+}
